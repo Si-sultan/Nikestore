da5b83418ec98c270fe81d34fe42ea56ecbefd29
sultanidrisi <isultanjaved@gmail.com>
2023-11-13 18:36:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import "./components/Navbar"
+import { Hero, CustomerReviews, Footer, Subscribe, SuperQuality,
+SpecialOffers,Services,PopularProducts } from "./sections";
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <main className="relative">
+    <Navbar />
+    <section className="xl:padding-l wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffers />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding-x padding-t pb-8 bg-black">
+      <Footer />
+    </section>
+    
+    </main>
+  );
+};
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+
+export{
+    Hero,
+    PopularProducts,
+    Footer,
+    CustomerReviews,
+    Subscribe,
+    SpecialOffers,
+    SuperQuality,
+    Services
+}
